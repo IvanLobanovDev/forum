ec130e6b27dbc1b8c2bd7096074b359c16672161
ivan.lobanov <f9.slam@gmail.com>
2024-02-28 21:08:31 +0200

    Auth configurated except updatePost and deletePost methods

--- src/main/java/telran/java51/security/AuthorizationConfiguration.java
+++ src/main/java/telran/java51/security/AuthorizationConfiguration.java
@@ -2,9 +2,16 @@ package telran.java51.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authorization.AuthoritiesAuthorizationManager;
+import org.springframework.security.authorization.AuthorizationDecision;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.access.expression.WebExpressionAuthorizationManager;
+
+import telran.java51.accounting.model.Role;
 
 @Configuration
 public class AuthorizationConfiguration {
@@ -16,6 +23,25 @@ public class AuthorizationConfiguration {
 		http.authorizeHttpRequests(authorize -> authorize
 				.requestMatchers("/account/register", "/forum/posts/**")
 					.permitAll()
+				.requestMatchers("/account/user/{login}/role/{role}")
+					.hasRole(Role.ADMINISTRATOR.name())
+				.requestMatchers(HttpMethod.PUT, "/account/user/{login}")
+//				Проверка что логин и объект authentication совпадают
+					.access(new WebExpressionAuthorizationManager("#login == authentication.name"))
+				.requestMatchers(HttpMethod.DELETE, "/account/user/{login}")
+					.access(new WebExpressionAuthorizationManager("#login == authentication.name or hasRole('ADMINISTRATOR')"))
+				.requestMatchers(HttpMethod.POST, "/forum/post/{author}")
+					.access(new WebExpressionAuthorizationManager("#author == authentication.name"))
+				.requestMatchers(HttpMethod.PUT, "/forum/post/*/comment/{author}")
+					.access(new WebExpressionAuthorizationManager("#author == authentication.name"))
+//					Кастомный метод сверяющий автора поста и имя  authentication совпадают
+				.requestMatchers(HttpMethod.PUT, "/forum/post/{id}")
+					.access(new WebExpressionAuthorizationManager("@customSecurity.checkPostAuthor(#id, authentication.name)"))
+//				.access(new AuthoritiesAuthorizationManager<T>() {
+//					
+//					@Override
+//					public AuthorizationDecision check(Supplier<Authrntication> )
+//				})
 				.anyRequest()
 					.authenticated()
 				
@@ -25,4 +51,5 @@ public class AuthorizationConfiguration {
 
 }
 
-
+//Реализовать методы UpdatePost и DeletePost
+//поправить чтобы роли можно было не только капсом писать
---
+++ src/main/java/telran/java51/security/CustomWebSecurity.java
@@ -0,0 +1,23 @@
+package telran.java51.security;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+import telran.java51.post.dao.PostRepository;
+import telran.java51.post.model.Post;
+
+// Можем указать как будет называться компонент в апп контексте
+@Service("customSecurity")
+//@Service
+@RequiredArgsConstructor
+public class CustomWebSecurity {
+	
+	final PostRepository postRepository;
+	
+	public boolean checkPostAuthor(String postId, String userName) {
+		Post post = postRepository.findById(postId).orElse(null);
+		return post != null && userName.equals(post.getAuthor());
+//		return userName.equals(post.getAuthor());
+	}
+
+}
